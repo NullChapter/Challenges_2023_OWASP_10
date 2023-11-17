2c44116ae2a59cd68f23df86904bfab805a4a35c
Ranjith Ashok <119282480+ranjithashok003@users.noreply.github.com>
2023-11-17 23:25:03 +0530

    Update e0x01.php

--- Security Misconfiguration (XML External Entity (XXE))/hard/e0x01.php
+++ Security Misconfiguration (XML External Entity (XXE))/hard/e0x01.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>XXE 0x01</title>
-    <link href="../XXE/assets/bootstrap.min.css" rel="stylesheet">
-    <link href="../XXE/assets/custom.css" rel="stylesheet">
+    <link href="../assets/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/custom.css" rel="stylesheet">
 </head>
 
 <body>
