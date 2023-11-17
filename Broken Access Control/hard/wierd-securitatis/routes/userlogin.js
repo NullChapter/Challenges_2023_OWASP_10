1921160c2f4da63b0ed46b43862c7a82f29408a3
srivenkatharsha <srivenkatharsha1@gmail.com>
2023-11-18 00:01:41 +0530

    user undefined bug fixed

--- Broken Access Control/hard/wierd-securitatis/routes/userlogin.js
+++ Broken Access Control/hard/wierd-securitatis/routes/userlogin.js
@@ -6,6 +6,7 @@ module.exports = async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const user = await getUserByUsername(username);
+    if(!user) return res.json({error: "invalid login"});
     if(!bcrypt.compare(password, user.password)){
         return res.status(403).json(
             {
